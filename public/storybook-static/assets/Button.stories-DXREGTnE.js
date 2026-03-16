041b1d0921e15a09bee09da3d213360577684386
smilv <owou@icloud.com>
2026-03-17 01:03:54 +0800

    add task

---
+++ public/storybook-static/assets/Button.stories-DXREGTnE.js
@@ -0,0 +1,21 @@
+import{_ as o}from"./Button-py1yjo-V.js";import"./iframe-B7wCo_33.js";import"./preload-helper-PPVm8Dsz.js";const{fn:t}=__STORYBOOK_MODULE_TEST__,m={title:"Example/Button",component:o,tags:["autodocs"],argTypes:{size:{control:"select",options:["small","medium","large"]},backgroundColor:{control:"color"}},args:{primary:!1,onClick:t()}},r={args:{primary:!0,label:"Button"}},a={args:{primary:!1,label:"Button"}},e={args:{label:"Button",size:"large"}},s={args:{label:"Button",size:"small"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    primary: true,
+    label: 'Button'
+  }
+}`,...r.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    primary: false,
+    label: 'Button'
+  }
+}`,...a.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Button',
+    size: 'large'
+  }
+}`,...e.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Button',
+    size: 'small'
+  }
+}`,...s.parameters?.docs?.source}}};const p=["Primary","Secondary","Large","Small"];export{e as Large,r as Primary,a as Secondary,s as Small,p as __namedExportsOrder,m as default};
---
+++ public/storybook-static/assets/formatter-EIJCOSYU-BSeQWRlN.js
@@ -0,0 +1,1 @@
+import{d as m,a as e}from"./blocks-BiCLgG7B.js";import{b as o}from"./iframe-B7wCo_33.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";var t=o(e(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:m(r));export{s as formatter};
